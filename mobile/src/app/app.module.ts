43dfae8c973011b1f271712509acfb5630dda856
Liam Read <liam.read@students.plymouth.ac.uk>
2018-11-27 16:04:11 +0000

    mobile app project

---
+++ mobile/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {firebaseConfig} from './credentials';
+
+@NgModule({
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ mobile/src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCMDCPz8eEOtajD5aQYFrmdouJMzkkmwZ8",
+    authDomain: "mobile-data-collection-32fd9.firebaseapp.com",
+    databaseURL: "https://mobile-data-collection-32fd9.firebaseio.com",
+    projectId: "mobile-data-collection-32fd9",
+    storageBucket: "mobile-data-collection-32fd9.appspot.com",
+    messagingSenderId: "306366160845"
+};
