8fd12ce2aeda9b965231a2fd0eb8ad6a8af723cd
Liam Read <liam@liam.pro>
2019-01-09 11:38:44 +0000

    Changes before setting to public mode

--- mobile/src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCMDCPz8eEOtajD5aQYFrmdouJMzkkmwZ8",
-    authDomain: "mobile-data-collection-32fd9.firebaseapp.com",
-    databaseURL: "https://mobile-data-collection-32fd9.firebaseio.com",
-    projectId: "mobile-data-collection-32fd9",
-    storageBucket: "mobile-data-collection-32fd9.appspot.com",
-    messagingSenderId: "306366160845"
-};
